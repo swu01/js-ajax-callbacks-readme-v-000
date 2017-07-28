2d7672b28ee03582cd25a2ba387e0a2605462441
Sammy Wu <wu.sammy@gmail.com>
2017-07-28 04:48:18 +0000

    Done.

---
+++ script.js
@@ -0,0 +1,7 @@
+$.get("this_doesnt_exist.html", function(data) {
+  // This will not be called because the .html file request doesn't exist
+  doSomethingGood();
+}).fail(function(error) {
+  // This is called when an error occurs
+  console.log('Something went wrong: ' + error);
+});
